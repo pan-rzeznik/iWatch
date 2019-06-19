9bd4d3156d6f00309d15b271f5c7c18d92b0f4dd
Damian <damian.marusarz@outlook.com>
2019-06-19 19:11:32 +0200

    Add RWD

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+// let toggler = `     <div class="toggler">
+//                     <div class="line"></div>
+//                     <div class="line"></div>
+//                     <div class="line"></div>
+//                     </div>`;
+
+// let place = document.querySelector('.page-container');
+// function abc(){
+//     if(window.innerWidth <= 900) {
+//         place.insertAdjacentHTML('beforeend', toggler);
+//         console.log('gotowe');
+        
+//     }
+   
+    
+// }
+
+// window.addEventListener('resize', abc);
